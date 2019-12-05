37c4bc8428933c47618379e37d51ef9d242c69b9
wglugla <gluglawojciech@gmail.com>
2019-12-05 13:01:46 +0100

    Create basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,35 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './App.css';
 import SwitchLanguageButton from './components/SwitchLanguageButton';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Example1 from './containers/Example1/Example1';
 import Example2 from './containers/Example2/Example2';
 
 const App: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <>
-      <h1>{t('Hello, world!')}</h1>
-      <ul>
-        <li>
-          <SwitchLanguageButton code={'pl'} />
-        </li>
-        <li>
-          <SwitchLanguageButton code={'en'} />
-        </li>
-      </ul>
-      <Example1 />
-      <Example2 />
-    </>
+    <Router>
+      <Link to='/'>Home</Link>
+      <Link to='/register'>Register</Link>
+      <Switch>
+        <Route path='/register'>
+          <p> Register</p>
+        </Route>
+        <Route path='/'>
+          <h1>{t('Hello, world!')}</h1>
+          <ul>
+            <li>
+              <SwitchLanguageButton code={'pl'} />
+            </li>
+            <li>
+              <SwitchLanguageButton code={'en'} />
+            </li>
+          </ul>
+          <Example1 />
+          <Example2 />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
