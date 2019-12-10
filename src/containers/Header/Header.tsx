a9f0d38e6695de84a8d78225c6d79a0497044d38
wglugla <gluglawojciech@gmail.com>
2019-12-10 19:08:02 +0100

    Fix useSelector in Header container

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,31 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import GoogleLoginButton from "../../components/GoogleLoginButton/GoogleLoginButton";
-import { useDispatch, useSelector } from "react-redux";
-import * as actions from "../Registration/actions";
-import SwitchLanguageButton from "../../components/SwitchLanguageButton";
-import { UserDataType } from "../Registration/types";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleLoginButton from '../../components/GoogleLoginButton/GoogleLoginButton';
+import { useDispatch, useSelector } from 'react-redux';
+import * as actions from '../Registration/actions';
+import SwitchLanguageButton from '../../components/SwitchLanguageButton';
+import { StoreType } from '../../store';
 
 const Header: React.FC = () => {
-  const accountInfo = useSelector((state: UserDataType) => state);
-
-  //Tutaj symulowanie stanu zalogowania
-  accountInfo.registrationInProgress = false;
+  const registrationInProgress = useSelector((state: StoreType) => state.user.registrationInProgress);
 
   const dispatcher = useDispatch();
   const signUpWithUserCode = (code: string) => {
     dispatcher(actions.signUpUserRequest('test'));
   };
 
-  const content = accountInfo.registrationInProgress ? (
+  const content = registrationInProgress ? (
     <h1>Header</h1>
   ) : (
     <div>
       <h1>Header</h1>
-      <Link to="/">Home</Link>
+      <Link to='/'>Home</Link>
       <GoogleLoginButton signUpUserWithCode={signUpWithUserCode} />
-      <SwitchLanguageButton code="pl" />
-      <SwitchLanguageButton code="en" />
+      <SwitchLanguageButton code='pl' />
+      <SwitchLanguageButton code='en' />
     </div>
   );
 
