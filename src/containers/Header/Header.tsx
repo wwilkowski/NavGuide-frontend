2532a9df8d174a1fd8829a4c3731f7281b3362de
Oskasieg <siegkrzysztof@gmail.com>
2019-12-10 18:22:55 +0100

    Create Header component if user isn't logged in

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import GoogleLoginButton from "../../components/GoogleLoginButton/GoogleLoginButton";
+import { useDispatch, useSelector } from "react-redux";
+import * as actions from "../Registration/actions";
+import SwitchLanguageButton from "../../components/SwitchLanguageButton";
+import { UserDataType } from "../Registration/types";
 
-const Header = () => {
-  return (
+const Header: React.FC = () => {
+  const accountInfo = useSelector((state: UserDataType) => state);
+
+  //Tutaj symulowanie stanu zalogowania
+  accountInfo.registrationInProgress = false;
+
+  const dispatcher = useDispatch();
+  const signUpWithUserCode = (code: string) => {
+    dispatcher(actions.signUpUserRequest);
+  };
+
+  const content = accountInfo.registrationInProgress ? (
+    <h1>Header</h1>
+  ) : (
     <div>
       <h1>Header</h1>
+      <Link to="/">Home</Link>
+      <GoogleLoginButton signUpUserWithCode={signUpWithUserCode} />
+      <SwitchLanguageButton code="pl" />
+      <SwitchLanguageButton code="en" />
     </div>
   );
+
+  return <>{content}</>;
 };
 
 export default Header;
