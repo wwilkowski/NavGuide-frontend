bbd5943939dbb472da2f9492ed1a16d5059b8b6e
wglugla <gluglawojciech@gmail.com>
2020-04-11 12:24:43 +0200

    Rewrite Checkbox component's styles to makeStyles

--- src/shared/Checkbox.jsx
+++ src/shared/Checkbox.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { Field } from 'formik';
-import styles from './Checkbox.module.scss';
 import { useTranslation } from 'react-i18next';
-import { Typography } from '@material-ui/core';
+import { Typography, makeStyles } from '@material-ui/core';
 
-const CheckboxComponent = props => {
+const useStyles = makeStyles({
+  hiddenField: {
+    display: 'none',
+    '&:checked + label': {
+      backgroundColor: '#e64740',
+      color: '#ffffff',
+    },
+  },
+  label: {
+    border: '1px solid #dbdbdb',
+    borderRadius: '5px',
+    color: '#333333',
+    padding: '0.3rem 0.8rem',
+    cursor: 'pointer',
+    background: '#ffffff',
+    transition: 'color 0.2s, background-color 0.2s',
+    whiteSpace: 'nowrap',
+  },
+});
+
+const CheckboxComponent = (props) => {
   const { t } = useTranslation();
+  const classes = useStyles();
+
   return (
     <Field name={props.name}>
       {({ field, form }) => (
@@ -15,11 +36,11 @@ const CheckboxComponent = props => {
             type='checkbox'
             name={props.name}
             value={props.valueKey}
-            className={styles.checkboxField}
+            className={classes.hiddenField}
             checked={field.value && field.value.includes(props.valueKey) ? true : false}
             onChange={() => {
               if (field.value && field.value.includes(props.valueKey)) {
-                const newInterests = field.value.filter(interest => interest !== props.valueKey);
+                const newInterests = field.value.filter((interest) => interest !== props.valueKey);
                 form.setFieldValue(props.name, newInterests);
               } else {
                 const nextValue = field.value ? [...field.value, props.valueKey] : [props.valueKey];
@@ -27,7 +48,7 @@ const CheckboxComponent = props => {
               }
             }}
           />
-          <Typography component='label' variant='subtitle1' htmlFor={props.value} className={styles.checkboxLabel}>
+          <Typography component='label' variant='subtitle1' htmlFor={props.value} className={classes.label}>
             {t(props.value)}
           </Typography>
         </>
